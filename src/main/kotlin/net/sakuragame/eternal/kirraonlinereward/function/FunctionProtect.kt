6d8d54de987a247cdb59e41f3c373bf0f636e5a5
kirraObj <c@skymc.me>
2022-07-21 14:23:57 +0800

    update.

--- src/main/kotlin/net/sakuragame/eternal/kirraonlinereward/function/FunctionProtect.kt
+++ src/main/kotlin/net/sakuragame/eternal/kirraonlinereward/function/FunctionProtect.kt
@@ -1,6 +1,5 @@
 package net.sakuragame.eternal.kirraonlinereward.function
 
-import ink.ptms.zaphkiel.api.event.ItemFullyLoadEvent
 import org.bukkit.event.player.PlayerLoginEvent
 import taboolib.common.platform.event.SubscribeEvent
 
@@ -17,8 +16,8 @@ object FunctionProtect {
 
     @SubscribeEvent
     fun e(e: PlayerLoginEvent) {
-//        if (!loaded) {
-//            e.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Server is loading.")
-//        }
+        if (!loaded) {
+            e.disallow(PlayerLoginEvent.Result.KICK_OTHER, "Server is loading.")
+        }
     }
 }
